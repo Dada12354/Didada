b4731651d9ba2082c7ed1570c7a0cc559350d9f8
323 <323@323-11>
2021-10-05 11:38:04 +0300

    nomer 1

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -10,11 +10,22 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            double z, a, b, h, p;
-            a = 3;
-            b = 7;
-            h = 5;
-            p = 3 * 4;
+            double z, p;
+            Console.WriteLine("Введите a=");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("Введите b=");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b=" + b);
+            Console.WriteLine("Введите h=");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("h=" + h);
+            Console.ReadKey();
+            p = 3 * (Math.Pow(a, 2) + Math.Pow(b, 2));
+            z = ((Math.PI * h) / 6) * (p + Math.Pow(h, 2));
+            z = Math.Round(z, 3);
+            Console.WriteLine("z=" + z);
+            Console.ReadKey();
 
         }
     }
