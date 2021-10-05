511b1b328faf6fc067babb577c40fd16e9a7c077
323 <323@kp15.lan>
2021-10-05 11:04:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z, a, b, h, p;
+            a = 3;
+            b = 7;
+            h = 5;
+            p=3*()
+
+        }
+    }
+}
